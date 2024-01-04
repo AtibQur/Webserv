7a1a78ec8cf8833467170197d50694de53758db1
Raav Koper <rkoper@f1r2s9.codam.nl>
2024-01-04 13:56:47 +0100

    added stuff

--- src/configuration/Config.cpp
+++ src/configuration/Config.cpp
@@ -243,18 +243,6 @@ std::string trimTillLastSlash(std::string path) {
     return (path.substr(0, std::distance(path.begin(), it)));
 }
 
-void Config::setDefaultErrorPages() {
-    _error_pages["400"] = "../../docs/error_pages/400.html";
-    _error_pages["401"] = "../../docs/error_pages/401.html";
-    _error_pages["403"] = "../../docs/error_pages/403.html";
-    _error_pages["404"] = "../../docs/error_pages/404.html";
-    _error_pages["405"] = "../../docs/error_pages/405.html";
-    _error_pages["413"] = "../../docs/error_pages/413.html";
-    _error_pages["500"] = "../../docs/error_pages/500.html";
-    _error_pages["501"] = "../../docs/error_pages/501.html";
-    _error_pages["505"] = "../../docs/error_pages/505.html";
-}
-
 Location Config::getLocation(std::string path) 
 {
     if (path == "/")
