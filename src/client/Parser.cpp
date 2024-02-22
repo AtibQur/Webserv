8550cfd349025dc194ceef3655f3dedd285fdbf4
Raav Koper <rkoper@f1r2s9.codam.nl>
2024-02-22 16:37:02 +0100

    fixed infinite loop

--- src/client/Parser.cpp
+++ src/client/Parser.cpp
@@ -51,7 +51,6 @@ int Client::parseRequest(std::string request, ssize_t post)
     std::stringstream httpRequest(request);
     std::string tmp;
 
-    std::cout << "Request: " << request << std::endl;
 
     if (!checkRequestLine(request))
     {
